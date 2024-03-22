0d46f30ab3ce027dced7296ed2f5193d27a8ba0b
Ransika <57299043+RansikaP@users.noreply.github.com>
2024-03-22 16:18:11 -0400

    6th stage

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,10 +1,6 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.Callable;
 
 public class Server {
     private int port;
@@ -19,40 +15,11 @@ public class Server {
 
     public void start() {
         try {
-            HttpParser parser = new HttpParser(clientSocket.getInputStream());
-            parser.parseRequest();
-
-            if (parser.getRequestURL().equals("/"))
-                this.httpResponseOK();
-            else if (parser.getRequestURL().startsWith("/echo")) {
-                String body = parser.getRequestURL().substring(6);
-                this.httpResponseText(body);
-            } else if (parser.getRequestURL().startsWith("/user-agent")) {
-                String body = parser.getHeader("User-Agent");
-                this.httpResponseText(body);
-            }
-            else
-                this.httpResponseNotFound();
+            ServerHandler handler = new ServerHandler(clientSocket.getInputStream(), clientSocket.getOutputStream());
+            handler.run();
         } catch (IOException e) {
             System.out.println("Could not start server: " + e.getMessage());
         }
-    }
 
-    private void httpResponseOK() throws IOException {
-        clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-        clientSocket.getOutputStream().flush();
-    }
-
-    private void httpResponseNotFound() throws IOException {
-        clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
-        clientSocket.getOutputStream().flush();
-    }
-
-    private void httpResponseText(String body) throws IOException {
-        clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n".getBytes());
-        clientSocket.getOutputStream().write("Content-Type: text/plain\r\n".getBytes());
-        clientSocket.getOutputStream().write(String.format("Content-Length: %d\r\n\r\n", body.length()).getBytes());
-        clientSocket.getOutputStream().write(body.getBytes());
-        clientSocket.getOutputStream().flush();
     }
 }
--- src/main/java/ServerHandler.java
+++ src/main/java/ServerHandler.java
@@ -1,2 +1,53 @@
-public class ServerHandler {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class ServerHandler implements Runnable {
+    private InputStream input;
+    private OutputStream output;
+
+    public ServerHandler(InputStream input, OutputStream output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    @Override
+    public void run() {
+        try {
+            HttpParser parser = new HttpParser(input);
+            parser.parseRequest();
+
+            if (parser.getRequestURL().equals("/"))
+                this.httpResponseOK();
+            else if (parser.getRequestURL().startsWith("/echo")) {
+                String body = parser.getRequestURL().substring(6);
+                this.httpResponseText(body);
+            } else if (parser.getRequestURL().startsWith("/user-agent")) {
+                String body = parser.getHeader("User-Agent");
+                this.httpResponseText(body);
+            }
+            else
+                this.httpResponseNotFound();
+        } catch (IOException e) {
+            System.out.println("Could not start handler: " + e.getMessage());
+        }
+    }
+
+    private void httpResponseOK() throws IOException {
+        output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+        output.flush();
+    }
+
+    private void httpResponseNotFound() throws IOException {
+        output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+        output.flush();
+    }
+
+    private void httpResponseText(String body) throws IOException {
+        output.write("HTTP/1.1 200 OK\r\n".getBytes());
+        output.write("Content-Type: text/plain\r\n".getBytes());
+        output.write(String.format("Content-Length: %d\r\n\r\n", body.length()).getBytes());
+        output.write(body.getBytes());
+        output.flush();
+    }
 }
